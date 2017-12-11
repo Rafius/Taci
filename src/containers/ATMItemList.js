23c5674491cbb1a93e2c0824314ab75948570b6b
Rafael Puyol Fernandez <rafius93@gmail.com>
2017-12-11 23:05:15 +0000

    Added message to ws

--- src/containers/ATMItemList.js
+++ src/containers/ATMItemList.js
@@ -4,8 +4,11 @@ import ATMItem from './ATMItem'
 class ATMItemList extends Component{
   componentDidMount(){
      this.ws = new WebSocket('ws://localhost:1716/component')
-     this.ws.onopen = e => console.log('hola')
-     this.ws.onmessage = e => this.setState({ users: Object.values(JSON.parse(e.data)) })
+     this.ws.onopen = e => this.ws.send('{"component":"taciPoc", "request":{"method":"doConnect","fields":[{"type": "CALLBACK","className": "com.caixabank.tas.ml.component.debug.tacipoc.ITaciPocCallback"}]}}')
+     this.ws.onmessage = e => {
+       debugger
+       this.setState({ users: Object.values(JSON.parse(e.data)) })
+     }
      this.ws.onerror = e => this.setState({ error: 'WebSocket error' })
      this.ws.onclose = e => !e.wasClean && this.setState({ error: `WebSocket error: ${e.code} ${e.reason}` })
   }
